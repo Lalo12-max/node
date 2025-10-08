2d19c5afc30c22bce0d35a3547cd0a00e2dc3b81
Lalo12-max <144458851+Lalo12-max@users.noreply.github.com>
2025-10-07 23:14:37 -0600

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
-const index = require('./testindex');
+import hash from "./testindex.js";
 
-test('Filter even numbers', () => {
-    const message = "contraseña";
-    const filteredNumbers = index(message);
-    expect(filteredNumbers).toEqual("559a0612917c8c516c7980c38b376cf2bb2387ae1b6944b3b32245ff4f2bad4d5db3811f02cff394fb62fa3feefd03e2eb83ba7bcca72a912bb87ea609c424da");
+test("Verificar hash", () => {
+  const mensaje = "contraseña";
+  const hashTest = hash(mensaje);
+  expect(hashTest).toBe("edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5");
 });
